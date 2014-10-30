581baf3bbf3bc4f5a6b748f90fcfb83deae5b6f6
tomaszjureczko <tomasz.jureczko@gmail.com>
2014-10-30 19:31:53 +0000

    init

---
+++ asics-links.js
@@ -0,0 +1,38 @@
+var links = {
+local: {
+uk: http://asicsuk.local:9001/asicsstorefront/
+australia: http://asicsau.local:9001/asicsstorefront
+germany: http://asicsde.local:9001/de/de-de
+spain: http://asicses.local:9001/es-es/search/-
+france: http://asicsfr.local:9001/fr-fr/search/-
+us: http://asics.local:9001/asicsstorefront/
+japan: http://asicsjp.local:9001/
+},
+test: {
+uk: http://asicsuk-test2.amaze.com:9001/asicsstorefront/
+australia: http://asicsau-test2.amaze.com:9001/asicsstorefront/
+germany: http://asicsde-test2.amaze.com:9001/asicsstorefront/de/de-de/
+spain: extend as per local address using es and /es-es etc..
+france: extend  as per local address
+us: http://asics-test2.amaze.com:9001/asicsstorefront/
+japan: http://asicsjp-test2.amaze.com:9001/asicsstorefront/
+},
+staging: {
+uk: http://asicsuk-staging2.amaze.com:9001/asicsstorefront/
+australia: http://asicsau-staging2.amaze.com:9001/asicsstorefront/
+germany: http://asicsde-staging2.amaze.com:9001/de-de/search/-
+spain: extend as per local address
+france: extend  as per local address
+us: http://asics-staging2.amaze.com:9001/
+japan: http://asicsjp-staging2.amaze.com:9001/asicsstorefront/
+},
+ ecstage: {
+uk: http://www.hybris.ecstage-uk.asics.com/men/cat/gendermen
+australia:  extend as per local address
+germany: http://www.hybris.ecstage.asics.com/de/de-de/search/-
+spain:  extend  as per local address
+france:  extend  as per local address
+us: http://www.hybris.ecstage-ac.asics.com/men/cat/gendermen
+japan: etc
+}
+}
